51d67b13d513984c9d2d0e5d6b6250f93dfe084d
liverpoolfc88 <52122299+liverpoolfc88@users.noreply.github.com>
2020-07-21 17:22:46 +0500

    sasas
    asas

--- yii2-basic-template/_protected/translations/sr/yii.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    '{attribute} cannot be blank.' => 'polje {attribute} ne sme biti prazno.',
-    'The verification code is incorrect.' => 'Antispam kod nije tačan.',
-    'Home' => 'Početna',
-    'You are not allowed to perform this action.' => 'Nije Vam dozvoljeno da to uradite.',
-    'The requested page does not exist.' => 'Tražena stranica ne postoji',
-];
